acbb634be55ac4fa449938b1f27e34bf7b1a30af
Bharat Tiwari <kkaphm@live.com>
2015-08-01 19:58:26 -0400

    Abstract Factory Design Pattern - Car Info Agency - C# Windows forms project w VS solution
    Abstract Factory Design Pattern - Car Info Agency - C# Windows forms
    project w VS solution

---
+++ dotNetExample/dotNetExample_CarInfoAgency/carCompany.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carInfoAgencyExample
+{
+    public abstract class carCompany {
+	    public string manufacturerName{get; set;}
+        public abstract List<carModel> getCarModels();
+        //public abstract List<carSales> getCarSales(String year, String modelName = null);
+    }
+}
---
+++ dotNetExample/dotNetExample_CarInfoAgency/carModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carInfoAgencyExample
+{
+    public abstract class carModel {
+            public string name {get; set;}
+
+            public string Hybrid
+            {
+                get { return isHybrid().ToString(); }
+                
+            }
+
+            public abstract Boolean isHybrid();
+        }
+}
